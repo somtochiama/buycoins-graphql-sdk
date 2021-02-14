2a96be528c9ea6ddaca1256f23516988a5c1c1c3
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2021-02-14 08:02:26 +0100

    Adds more tests
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- test/api.test.js
+++ test/api.test.js
@@ -1,8 +1,30 @@
 import Api from '../src/packages/api'
+import { GraphQLClient, request } from 'graphql-request'
+import { mockClient } from "./testdata/testdata"
+
+jest.mock('graphql-request')
 
 describe('Api', () => {
     test('throws error when client is undefined', () => {
         const api = new Api() // api initialized with no client
         return expect(api.query()).rejects.toThrowError(/Missing client/)
+    })
+
+    test('request is called successfully', () => {
+        const client = new GraphQLClient("test.buycoins.sdk")
+        const api = new Api(client)
+        const query = `
+            createDepositAccount(accountName: "tony stark") {
+                accountNumber
+                accountName
+                accountType
+                bankName
+                accountReference
+            }
+        `
+        
+        api.query(query)
+        // return expect(api.client.request).toHaveBeenCalled()
+        return expect(api.client.request).toHaveBeenCalledWith(query, undefined)
     })
 })
